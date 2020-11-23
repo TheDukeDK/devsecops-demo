4d35bedfa44ad419f433e071c45625ee51d26878
Timothy Harris <timothy.harris@eficode.com>
2020-11-23 15:58:40 +0100

    Just make the empty pipeline green for now.

--- pipelines/devsecops.groovy
+++ pipelines/devsecops.groovy
@@ -1,28 +1,16 @@
 #!groovy
-
 pipeline {
-
-    /*
-    agent {
-        node {
-            label 'windows'
-            customWorkspace "${JOB_NAME}"
-        }
-    }
-    */
-    //parameters {}
-    
+    agent any 
     environment {
         CHECKOUT = "checkout"
-        BRANCH = ""
-        REPO_URL = ""
-        REPO_CREDS = ""
-        DEPLOY_ID = ""
+        BRANCH = "main"
+        REPO_URL = "git@gitlab.local.net:root/test.git"
+        REPO_CREDS = "Mine"
+        DEPLOY_ID = "Dummy"
     }
-    
     options {
         disableConcurrentBuilds()
-        buildDiscarder(logRotator(numToKeepStr: '10'))
+        buildDiscarder(logRotator(numToKeepStr: '1'))
     }
     stages {
         stage('Checkout') {
@@ -40,60 +28,45 @@ pipeline {
         }
         stage('Build & Analyse') {
             parallel {
-                stage('Build'){ steps {dir("${CHECKOUT}"){sh 'echo build'}}}
-                stage('Unit Test'){ steps {dir("${CHECKOUT}"){sh 'echo build'}}}
-                stage('Static Analysis'){ steps {dir("${CHECKOUT}"){sh 'echo build'}}}
-
+                    stage('Build'){ steps {dir("${CHECKOUT}"){sh 'echo build'}}}
+                    stage('Unit Test'){ steps {dir("${CHECKOUT}"){sh 'echo build'}}}
+                    stage('Static Analysis'){ steps {dir("${CHECKOUT}"){sh 'echo build'}}}
+                }
         }
         stage('Evaluate') {
-            steps {
-                script {
-                    dir("${CHECKOUT}") {
-                        timeout(time: 5, unit: 'MINUTES') {
-                            def qg = waitForQualityGate()
-                            if (qg.status != 'OK') {
-                                error "The SonarQube Quality Gate has failed with: ${qg.status}! Please go to ${SONAR_URL} for reference..."
-                            }
-                        }
-                    }
-                }
-            }
+            steps { dir("${CHECKOUT}") {sh "echo add SQ QG here!"}}
         }
         stage('Pack') {
-            steps {
-                dir("${CHECKOUT}") {
-                    sh "echo Pack or prepare artifact for deployment"
-                }
-            }
+            steps {dir("${CHECKOUT}"){sh "echo Pack or prepare artifact for deployment"}}
         }
-        stage ('Deploy') {
-            steps {
-                withCredentials([usernamePassword(credentialsId: "${DEPLOY_ID}", passwordVariable: 'PASSWORD', usernameVariable: 'USER_NAME')]) {
-                    dir("${CHECKOUT}") {}
-                }
-            }
-        }
+        stage ('Deploy') {steps {dir("${CHECKOUT}") {sh "echo Do a deploy here."}}}
         stage('Functional Tests') {
-            //when { expression {} }
             parallel {
-               stage('SAST') { steps { sh "Run Security Tests"} }
-
+               stage('SAST') { steps { sh "echo Run Security Tests"} }
+               stage('DAST') { steps { sh "echo Run Security Tests"} }
             }
         }
     }
     post {
         success {
             dir("${CHECKOUT}") {
+                sh "echo Do something on success!"
             }
         }
         unstable {
-            dir("${CHECKOUT}") {}
+            dir("${CHECKOUT}") {
+                sh "echo Do something on unstable!"
+            }
         }
         failure {
             dir("${CHECKOUT}") {
+                sh "echo Do something on failure!"
             }
         }
         always {
+            dir("${CHECKOUT}") {
+                sh "echo Do something on always here!"
+            }
         }
     }
 }
