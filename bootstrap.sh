2e129c56929fe5e6b9f811932e4e84e1cd9021d2
Timothy Harris <timothy.harris@eficode.com>
2020-12-03 14:26:06 +0100

    Cleanup output in bootstrap.sh script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,19 +13,20 @@ source ./sourceme
 docker-compose up -d
 
 # SonarQube ready?
-echo "Waiting for SonarQube to come up."
+echo "Waiting for SonarQube to come up..."
 while [[ "`curl -s -f -u admin:admin http://sonarqube.local.net/api/system/status | sed -n 's|.*"status":"\([^"]*\)".*|\1|p'`" != "UP" ]]; do
     sleep 2
 done;
 
 # Create Jenkins user
-curl -s -u admin:admin -X POST http://sonarqube.local.net/api/users/create\?login\=$JENKINS_ADMIN_USER\&local\=true\&name\=$JENKINS_ADMIN_USER\&password\=$JENKINS_ADMIN_PASSWORD
+echo "Creating Jenkins user and webhook..."
+curl -s -u admin:admin -X POST http://sonarqube.local.net/api/users/create\?login\=$JENKINS_ADMIN_USER\&local\=true\&name\=$JENKINS_ADMIN_USER\&password\=$JENKINS_ADMIN_PASSWORD > /dev/null 2>&1
+curl -s -u admin:admin -X POST http://sonarqube.local.net/api/user_groups/add_user\?login\=$JENKINS_ADMIN_USER\&name\=sonar-administrators > /dev/null 2>&1
+curl -s -u admin:admin -X POST http://sonarqube.local.net/api/webhooks/create\?name\=jenkins\&url\="http://jenkins.local.net/sonarqube-webhook/" > /dev/null 2>&1
 
-## Add jenkins to adminitsrator group and create a token we can add to Jenkins
-curl -s -u admin:admin -X POST http://sonarqube.local.net/api/user_groups/add_user\?login\=$JENKINS_ADMIN_USER\&name\=sonar-administrators
-curl -s -u admin:admin -X POST http://sonarqube.local.net/api/user_tokens/generate\?login\=$JENKINS_ADMIN_USER\&name\=jenkins
-
-curl -s -u admin:admin -X POST http://sonarqube.local.net/api/webhooks/create\?name\=jenkins\&url\="http://jenkins.local.net/sonarqube-webhook/"
+# Create a token for the jenkins user
+TOKEN=`curl -s -u admin:admin -X POST http://sonarqube.local.net/api/user_tokens/generate\?login\=$JENKINS_ADMIN_USER\&name\=jenkins | sed -n 's|.*"token":"\([^"]*\)".*|\1|p'`
+echo "Use this token value in Jenkins credential 'token-sonarqube': $TOKEN"
 
 # Must wait for gitlab to become healthy
 echo "Waiting for gitlab to be healthy. Be patient, may take a few minutes."
