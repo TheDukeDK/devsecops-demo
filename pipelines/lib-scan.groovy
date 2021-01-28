b93f57585e40e9586ff3c26ec752e26fd5ed3607
Timothy Harris <timothy.harris@eficode.com>
2021-01-28 17:01:21 +0100

    Remove static analysis, build stuff. Try specify two formats.

--- pipelines/lib-scan.groovy
+++ pipelines/lib-scan.groovy
@@ -7,31 +7,6 @@ pipeline {
         buildDiscarder(logRotator(numToKeepStr: '5'))
     }
     stages {
-        stage('eShop Source') { 
-            steps {
-                dir("sample_projects/eShopOnWeb"){
-                    // If you are using the Developer edition of SonarQube add the branch name -> /d:sonar.branch.name=${GIT_BRANCH}.
-                    withSonarQubeEnv('sonarqube.local.net'){sh "dotnet-sonarscanner begin /k:eShopOnWeb"}
-                    sh 'dotnet build eShopOnWeb.sln'
-                }
-            }
-        }
-        stage('Unit Tests'){
-            steps {
-                dir("sample_projects/eShopOnWeb"){
-                    sh 'dotnet test tests/UnitTests/UnitTests.csproj /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=cobertura'
-                }
-            }
-        }
-        stage('Static Analysis QG'){
-            steps {
-                dir("sample_projects/eShopOnWeb"){
-                    withSonarQubeEnv('sonarqube.local.net') { 
-                        sh "dotnet-sonarscanner end"
-                    }
-                }
-            }
-        }
         stage('Library Scans') {
             parallel {
                 stage('NPM Audit') 
@@ -44,8 +19,8 @@ pipeline {
                 }
                 stage('OWASP Dependency') {
                     steps {
-                        dir("sample_projects/eShopOnContainers") {
-                            sh 'dependency-check.sh --project "eShopOnContainers" --scan ./ -f ALL'
+                        dir("sample_projects/eShopOnContainers/src/Web/WebSPA") {
+                            sh 'dependency-check.sh --project "eShopOnContainers" --scan ./ -f XML,HTML'
                             dependencyCheckPublisher pattern: '**/dependency-check-report.*', 
                                 failedNewCritical: 1,
                                 failedNewHigh: 1,
