2e07b01d332c6dd6dad891c078623059e52594b9
Timothy Harris <timothy.harris@eficode.com>
2021-01-28 17:11:51 +0100

    Will it fail again with ALL formats.

--- pipelines/lib-scan.groovy
+++ pipelines/lib-scan.groovy
@@ -20,7 +20,7 @@ pipeline {
                 stage('OWASP Dependency') {
                     steps {
                         dir("sample_projects/eShopOnContainers/src/Web/WebSPA") {
-                            sh 'dependency-check.sh --project "eShopOnContainers" --scan ./ -f XML'
+                            sh 'dependency-check.sh --project "eShopOnContainers" --scan ./ -f ALL'
                             dependencyCheckPublisher pattern: 'dependency-check-report.xml', 
                                 failedNewCritical: 1,
                                 failedNewHigh: 1,
