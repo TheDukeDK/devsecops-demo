5c9c6e270768897cad39c8460a1e52cac34a6107
Timothy Harris <timothy.harris@eficode.com>
2021-01-28 21:03:55 +0100

    Leave the parser stuff there for record?

--- pipelines/lib-scan.groovy
+++ pipelines/lib-scan.groovy
@@ -12,21 +12,21 @@ pipeline {
                 stage('NPM Audit') 
                 {
                     steps {
-                        //script {
-                        //    // set up the parser
-                        //    def config = io.jenkins.plugins.analysis.warnings.groovy.ParserConfiguration.getInstance()
-                        //
-                        //    if(!config.contains('npm-audit')){
-                        //        def newParser = new io.jenkins.plugins.analysis.warnings.groovy.GroovyParser(
-                        //            'npm-audit',
-                        //            'NPM Audit Parser',
-                        //            '\w+\t(\S+)\t(\w+)\t(\S| )+\t((\S| )+)\t(\S+)\t(\S+)',
-                        //            'return builder.setFileName(matcher.group(7)).setCategory(matcher.group(4)).setMessage(matcher.group(6)).buildOptional()',
-                        //            "update\tlodash\tlow\tnpm update lodash --depth 9\tPrototype Pollution\thttps://npmjs.com/advisories/1523\telasticsearch>lodash\tN"
-                        //        )
-                        //        config.setParsers(config.getParsers().plus(newParser))
-                        //    }
-                        //}
+                        script {
+                            // set up the parser
+                            def config = io.jenkins.plugins.analysis.warnings.groovy.ParserConfiguration.getInstance()
+                        
+                            if(!config.contains('npm-audit')){
+                                def newParser = new io.jenkins.plugins.analysis.warnings.groovy.GroovyParser(
+                                    'npm-audit',
+                                    'NPM Audit Parser',
+                                    '\w+\t(\S+)\t(\w+)\t(\S| )+\t((\S| )+)\t(\S+)\t(\S+)',
+                                    'return builder.setFileName(matcher.group(7)).setCategory(matcher.group(4)).setMessage(matcher.group(6)).buildOptional()',
+                                    "update\tlodash\tlow\tnpm update lodash --depth 9\tPrototype Pollution\thttps://npmjs.com/advisories/1523\telasticsearch>lodash\tN"
+                                )
+                                config.setParsers(config.getParsers().plus(newParser))
+                            }
+                        }
                         dir("sample_projects/eShopOnContainers/src/Web/WebSPA") {
                             sh 'mkdir -p .tmp/npm'
                             sh 'npm audit --parseable > .tmp/npm/audit || true'
