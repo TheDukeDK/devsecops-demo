623aa2ed7f53f046fd7ed24c099bdda522e5c933
Timothy Harris <timothy.harris@eficode.com>
2021-02-04 09:47:03 +0100

    Update for jenkins script console snippets.

--- list-jenkins-plugins.groovy
+++ list-jenkins-plugins.groovy
@@ -1,13 +1,17 @@
 #!groovy
 
 /*
-    You can use this groovy snippet to list the plugins installed in Jenkins.
+    You can use these groovy snippets in Jenkins Script console.
     From Manage jenkins->Script Console paste and run the snippet.
-    Compare and update the plugins.txt file.
 */
 
+
+//  list the plugins installed in Jenkins. Compare and update the plugins.txt file.
 def pluginList = new ArrayList(Jenkins.instance.pluginManager.plugins)
 pluginList.sort { it.getShortName() }.each{
   plugin -> 
     println ("${plugin.getShortName()}:${plugin.getVersion()}")
 }
+
+// Set content security for pretty HTML reports
+System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "")
